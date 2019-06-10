2a1a5c64d8756bdf14a360371910b34dd95190bb
Connor Cowan <ccowan003@comcast.net>
2019-06-10 20:49:25 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,7 +8,7 @@ def my_all?(collection)
     i += 1
   end
   if block_return_values.include?(false)            #added an #include? to determine the return value of the #my_all?
-    false
+    false                                           #returns false when block condition is met
   else
     true                                             #returns true when block condition is met
   end
