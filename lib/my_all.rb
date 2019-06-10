bbece5606a39744f1e824bbce95f802eb83257ce
Connor Cowan <ccowan003@comcast.net>
2019-06-10 20:47:34 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,4 +7,9 @@ def my_all?(collection)
     block_return_values << yield(collection[i])        #yields each element in collection to the block. Sends whatever is passed in as the argument to the block.
     i += 1
   end
+  if block_return_values.include?(false)
+    false
+  else
+    true
+end
 end
