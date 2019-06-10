738124c1bc4294d473b4aab63e4d2d8f61ff7095
Connor Cowan <ccowan003@comcast.net>
2019-06-10 20:28:09 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
   i = 0
 
   while i < collection.length
-    yield(collection[1])        #yields each element in collection to the block
+    yield(collection[1])        #yields each element in collection to the block. Sends whatever is passed in as the argument to the block.
     i += 1
   end
 end
